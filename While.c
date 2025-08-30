2ad855e4d3a578d8171bdc9a4b3173ab93b40eb7
Sampath8358 <sampath7661@gmail.com>
2025-08-30 12:53:09 +0530

    Add files via upload

---
+++ While.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int pass = 1234;
+    int PassUser;
+    int attempts = 0;
+
+    while (attempts < 3) {
+        printf("Enter your password: ");
+        scanf("%d", &PassUser);
+
+        if (PassUser == pass) {
+            printf("Password is correct\n");
+            return 0;  // Exit program
+        } else {
+            attempts++;
+            if (attempts < 3) {
+                printf("Password is invalid\n");
+                printf("You have %d attempts left\n", 3 - attempts);
+            }
+        }
+    }
+
+    printf("Your account is blocked\n");
+    return 0;
+}
