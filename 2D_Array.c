bd60cda89fb1eab840003cce502801d09363a898
Sampath8358 <sampath7661@gmail.com>
2025-09-02 21:33:51 +0530

    Add files via upload

---
+++ 2D_Array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int arr[2][3];
+    printf("Enter the array elements: ");\
+    int sum = 0;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("below are the array elements \n");
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            sum = sum + arr[i][j];
+        }
+    }
+    printf("Sum all the elements in the array = %d",sum);
+    return 0;
+}
