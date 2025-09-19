bc41ce34368448f684ae96a1981bf1dfa950a3ed
Sampath8358 <sampath7661@gmail.com>
2025-09-19 11:49:18 +0530

    Add files via upload

--- linkedList.c
+++ linkedList.c
@@ -5,6 +5,8 @@ void insertBeg(int n);
 void travelForward();
 void insertEnd(int n);
 void insertAfetr(int n,int s);
+int deleteFirst();
+int deleteLast();
 
 struct node {
 	int data;
@@ -19,6 +21,8 @@ int main(){
     travelForward();
     printf("\n");
     insertAfetr(27,26);
+    deleteFirst();
+    deleteLast();
     travelForward();
 }
 
@@ -99,64 +103,46 @@ void insertAfetr(int n,int s){
     } //outer ifelse
 }// function
 
-void travelForward(){
-    struct node *current;
-    current = start;
+int deleteFirst(){
+    int x = -1;
+    struct  node *temp;
     if(start == NULL)
         printf("List is empty");
     else
     {
-        while(current!=NULL)
-        {
-            printf("%d ",current->data);
-            current = current->next;
-        }
+        temp = start;
+        x = temp->data;
+        start = start->next;
+        free(temp);
+        temp = NULL;
     }
-
+    return x;
 }
 
-void insertEnd(int n){
-    struct node *temp,*current;
-    temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = n;
-    temp->next = NULL;
-    if(start == NULL)
-    {
-        start = temp;
-    }  
-    else
-    {
-        current = start;
-        while(current->next != NULL)
-        {
-            current = current->next;
+int deleteLast() {
+    int x = -1;
+    if (start == NULL) {
+        printf("List is empty\n");
+    } else {
+        struct node *temp, *current;
+        
+        if (start->next == NULL) {
+            temp = start;
+            x = temp->data;
+            start = NULL;
+            free(temp);
+            temp = NULL;
+        } else {
+            current = start;
+            while (current->next->next != NULL) {
+                current = current->next;
+            }
+            temp = current->next;
+            x = temp->data;
+            current->next = NULL;
+            free(temp);
+            temp = NULL;
         }
-        current->next = temp;
     }
+    return x;
 }
-
-void insertAfetr(int n,int s){
-    struct  node *temp,*current;
-    if(start==NULL)
-        printf("List is empty");
-    else
-    {
-        temp = (struct node*)malloc(sizeof(struct node));
-        temp->data = n;
-        temp->next = NULL;
-        current = start;
-        while(current!=NULL && current->data!=s)
-        {
-            current = current->next;
-        }
-        if(current!=NULL)
-        {
-            temp->next = current->next;
-            current->next = temp;
-        }
-        else
-        {
-            printf("Element not found");
-        }
-    } //outer ifelse
-}// function
